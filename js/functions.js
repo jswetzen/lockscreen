e65600c56475398cc77b358c2b509833c7cf84b4
Johan Swetzén <jswetzen@gmail.com>
2014-07-07 18:06:40 +0200

    Add workaround for iOS canvas scaling issue

--- js/functions.js
+++ js/functions.js
@@ -40,7 +40,7 @@ function makeTextImage(text, width, height, useImage, background, angle) {
       ctx.rotate(radAngle);
       ctx.translate(-y,-x);
       scaling = scaleToFit(w, h, height, width);
-      ctx.drawImage(background, scaling.xOffset, scaling.yOffset, scaling.width, scaling.height);
+      drawImageIOSFix(ctx, background, 0, 0, w, h, scaling.xOffset, scaling.yOffset, scaling.width, scaling.height);
       ctx.translate(y,x);
       ctx.rotate(-radAngle);
       ctx.translate(-x,-y);
@@ -50,7 +50,7 @@ function makeTextImage(text, width, height, useImage, background, angle) {
       ctx.rotate(radAngle);
       ctx.translate(-x,-y);
       scaling = scaleToFit(w, h, width, height);
-      ctx.drawImage(background, scaling.xOffset, scaling.yOffset, scaling.width, scaling.height);
+      drawImageIOSFix(ctx, background, 0, 0, w, h, scaling.xOffset, scaling.yOffset, scaling.width, scaling.height);
       ctx.translate(x,y);
       ctx.rotate(-radAngle);
       ctx.translate(-x,-y);
@@ -189,3 +189,49 @@ function scaleToFit(srcWidth, srcHeight, destWidth, destHeight) {
 
   return result;
 }
+
+/**
+ * Detecting vertical squash in loaded image.
+ * Fixes a bug which squash image vertically while drawing into canvas for some images.
+ * This is a bug in iOS6 devices. This function from https://github.com/stomita/ios-imagefile-megapixel
+ * 
+ */
+function detectVerticalSquash(img) {
+  var iw = img.naturalWidth, ih = img.naturalHeight;
+  var canvas = document.createElement('canvas');
+  canvas.width = 1;
+  canvas.height = ih;
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+  var data = ctx.getImageData(0, 0, 1, ih).data;
+  // search image edge pixel position in case it is squashed vertically.
+  var sy = 0;
+  var ey = ih;
+  var py = ih;
+  while (py > sy) {
+    var alpha = data[(py - 1) * 4 + 3];
+    if (alpha === 0) {
+      ey = py;
+    } else {
+      sy = py;
+    }
+    py = (ey + sy) >> 1;
+  }
+  var ratio = (py / ih);
+  return (ratio===0)?1:ratio;
+}
+
+/**
+ * A replacement for context.drawImage
+ * (args are for source and destination).
+ */
+function drawImageIOSFix(ctx, img, sx, sy, sw, sh, dx, dy, dw, dh) {
+  var vertSquashRatio = detectVerticalSquash(img);
+  // Works only if whole image is displayed:
+  // ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh / vertSquashRatio);
+  // The following works correct also when only a part of the image is displayed:
+  ctx.drawImage(img, sx * vertSquashRatio, sy * vertSquashRatio, 
+      sw * vertSquashRatio, sh * vertSquashRatio, 
+      dx, dy, dw, dh );
+}
+
